08ae2722e1750e17fe89e9fe7450ede7047dfb3f
unidetvirtual-cyber <unidetvirtual@gmail.com>
2025-12-15 06:46:35 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,23 +3,24 @@ declare(strict_types=1);
 
 /**
  * Wrapper para soportar: /index.php?r=/news
- * Convierte r en la ruta real y luego carga Slim (public/index.php)
+ * Convierte r en PATH_INFO/REQUEST_URI y luego carga Slim (public/index.php)
  */
 if (isset($_GET['r'])) {
-    $r = (string)$_GET['r'];
-    if ($r === '') $r = '/';
-    if ($r[0] !== '/') $r = '/' . $r;
+    $r = (string) $_GET['r'];
+    if ($r === '') { $r = '/'; }
+    if ($r[0] !== '/') { $r = '/' . $r; }
 
-    // quitar r del query
-    unset($_GET['r']);
-    $query = http_build_query($_GET);
+    $qs = $_GET;
+    unset($qs['r']);
+    $query = http_build_query($qs);
 
-    $_SERVER['REQUEST_URI']  = $r . ($query ? ('?' . $query) : '');
+    $_SERVER['PATH_INFO'] = $r;
+    $_SERVER['ORIG_PATH_INFO'] = $r;
+    $_SERVER['REQUEST_URI'] = $r . ($query ? ('?' . $query) : '');
     $_SERVER['QUERY_STRING'] = $query;
-    $_SERVER['PATH_INFO']    = $r;
 
+    $_SERVER['PHP_SELF'] = '/index.php' . $r;
     $_SERVER['SCRIPT_NAME'] = '/index.php';
-    $_SERVER['PHP_SELF']    = '/index.php' . $r;
 }
 
 require __DIR__ . '/public/index.php';
